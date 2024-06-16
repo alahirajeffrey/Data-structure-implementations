a7a7d3631b15ebaee2936539eb5cc4ed657a2fdf
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-16 12:58:22 +0100

    added python stack functions

---
+++ stack/stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self) -> None:
+        self.items = []
+
+    def push():
+        pass
+
+    def pop():
+        pass
+
+    def size():
+        pass
+
+    def peek():
+        pass
+
+    def is_empty():
+        pass
