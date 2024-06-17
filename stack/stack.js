13ef805375c39aa8eaabd652f06ae39325776b3d
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-17 08:08:49 +0100

    function and comments

---
+++ stack/stack.js
@@ -0,0 +1,20 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  // check if stack is empty
+  isEmpty() {}
+
+  // push a new element to the stack
+  push() {}
+
+  // remove the last element placed in the stack
+  pop() {}
+
+  // look at the last element in the stack
+  peek() {}
+
+  // get the size of the stack
+  size() {}
+}
