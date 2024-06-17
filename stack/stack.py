66ebebb8b42abf6abeeba08e0ec11fe175fb666e
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-17 03:26:22 +0100

    added comment to functions

--- stack/stack.py
+++ stack/stack.py
@@ -2,17 +2,22 @@ class Stack:
     def __init__(self) -> None:
         self.items = []
 
+    ## push element to the top of the stack
     def push():
         pass
 
+    ## remove element from the top of the stack
     def pop():
         pass
 
+    ## get the size of the stack
     def size():
         pass
 
+    ## look at the last element in the stack
     def peek():
         pass
 
+    ## check if stack is empty
     def is_empty():
         pass
