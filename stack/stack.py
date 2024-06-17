0561e0d69c2c56fb0e97273e0cc95cf5fb402cfc
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-17 03:50:28 +0100

    completed stack implemetation in python

--- stack/stack.py
+++ stack/stack.py
@@ -3,21 +3,41 @@ class Stack:
         self.items = []
 
     ## push element to the top of the stack
-    def push():
-        pass
+    def push(self, item):
+        self.items.append(item)
+        return f"{item} added to stack"
 
     ## remove element from the top of the stack
-    def pop():
-        pass
+    def pop(self):
+        if not self.is_empty():
+            self.items.pop()
+            return f"Item removed from stack"
+        else:
+            return IndexError("Empty stack")       
 
     ## get the size of the stack
-    def size():
-        pass
+    def size(self):
+        return f"the stack has {len(self.items)} elements"
 
     ## look at the last element in the stack
-    def peek():
-        pass
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return IndexError("Empty stack")
 
     ## check if stack is empty
-    def is_empty():
-        pass
+    def is_empty(self):
+        return len(self.items) == 0
+        
+stack = Stack()
+
+print(stack.is_empty())
+print(stack.push(10))
+print(stack.push(12))
+print(stack.push(3))
+print(stack.size())
+print(stack.peek())
+print(stack.pop())
+print(stack.peek())
+print(stack.size())
